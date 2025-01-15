9e7bf77d7198cba5765a9d87f45214b0325dd216
pgomezzy <palp53412@gmail.com>
2025-01-15 20:21:37 -0300

    Adição do projeto

---
+++ core/api.py
@@ -0,0 +1,6 @@
+from ninja import NinjaAPI
+from shortener.api import shortener_router
+
+
+api = NinjaAPI()
+api.add_router('', shortener_router)
---
+++ shortener/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Links)
+admin.site.register(Clicks)
+# Register your models here.
